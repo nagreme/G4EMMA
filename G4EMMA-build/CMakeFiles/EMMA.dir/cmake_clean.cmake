cff3b0b23c91c0f6a3e98cc9f3577413065d0f1a
Nadège <nadpulgarvidal@gmail.com>
2017-11-02 16:10:41 -0700

    More cleanup

--- G4EMMA-build/CMakeFiles/EMMA.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/EMMA"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/EMMA.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
